0d31e1fde13a7e9ed974f329d58e2b268a175207
HassanAli <hassanali@A006-00229.local>
2023-10-08 00:03:31 +0500

    Update: env removed from gitignore

--- pakwheelsclone/settings.py
+++ pakwheelsclone/settings.py
@@ -77,16 +77,26 @@ TEMPLATES = [
 ]
 
 SECRET_KEY = config('SECRET_KEY')
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': config('DATABASE_ENGINE'),
         'NAME': config('DATABASE_NAME'),
         'USER': config('DATABASE_USER'),
         'PASSWORD': config('DATABASE_PASSWORD'),
         'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('DATABASE_NAME'),
+#         'USER': config('DATABASE_USER'),
+#         'PASSWORD': config('DATABASE_PASSWORD'),
+#         'HOST': config('DATABASE_HOST'),
+#     }
+# }
 # WSGI_APPLICATION = 'pakwheelsclone.wsgi.application'
 
 
