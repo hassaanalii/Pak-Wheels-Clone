04b839b7a8a3f96bf16649c7d1e3cc68d1e3f734
HassanAli <hassanali@A006-00229.local>
2023-10-08 01:10:14 +0500

    Update: Static files update in settings.py and urls.py

--- pakwheelsclone/urls.py
+++ pakwheelsclone/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings # new
+from  django.conf.urls.static import static #new
 
 urlpatterns = [
     path("__reload__/", include("django_browser_reload.urls")),
     path('admin/', admin.site.urls),
-    path('cars/', include("cars.urls"))
+    path('cars/', include("cars.urls")),
+    
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
